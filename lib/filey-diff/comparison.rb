c688f54faa63deb87c2c475f10d8e2cddd3b170c
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-10-08 21:21:25 +0300

    Refactor

--- lib/filey-diff/comparison.rb
+++ lib/filey-diff/comparison.rb
@@ -1,30 +1,35 @@
 module Filey
   class Comparison
     def self.list_outdated(data_source_a, data_source_b)
-      data_source_b.get_fileys.select { |b_item|
-        data_source_a.get_fileys.select { |a_item|
-          b_item.full_path == a_item.full_path and
-            b_item.last_modified < a_item.last_modified
-        }.length > 0
+      select_in_outer_array(data_source_b, data_source_a) { |b_item, a_item|
+        b_item.full_path == a_item.full_path and
+          b_item.last_modified < a_item.last_modified
       }
     end
 
     def self.list_changed(data_source_a, data_source_b)
-      data_source_b.get_fileys.select { |b_item|
-        data_source_a.get_fileys.select { |a_item|
-          b_item.full_path == a_item.full_path and
-            b_item.md5 != a_item.md5
-        }.length > 0
+      select_in_outer_array(data_source_b, data_source_a) { |b_item, a_item|
+        b_item.full_path == a_item.full_path and
+          b_item.md5 != a_item.md5
       }
     end
 
     def self.list_missing(data_source_a, data_source_b)
-      intersection = data_source_a.get_fileys.select { |a_item|
-        data_source_b.get_fileys.select { |b_item|
-          b_item.full_path == a_item.full_path
+      intersection = select_in_outer_array(data_source_a, data_source_b) do
+        |a_item, b_item|
+        b_item.full_path == a_item.full_path
+      end
+      data_source_a.get_fileys - intersection
+    end
+
+    private
+
+    def self.select_in_outer_array(outer, inner)
+      outer.get_fileys.select { |outer_item|
+        inner.get_fileys.select { |inner_item|
+          yield outer_item, inner_item
         }.length > 0
       }
-      data_source_a.get_fileys - intersection
     end
   end
 end
