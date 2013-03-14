56a435c86eac3543162d3208dec5e9b7b51ac0ba
Alex Marchant <alexjmarchant@gmail.com>
2013-03-13 20:00:35 -0700

    gzip file reading + test

--- lib/filey-diff/data-sources/aws_sdk_s3.rb
+++ lib/filey-diff/data-sources/aws_sdk_s3.rb
@@ -16,11 +16,25 @@ module Filey
             path = ''
             name = s3_object.key
           end
+
+          last_modified = nil
+          if (s3_object.head[:content_encoding] == "gzip")
+            tempfile = Tempfile.new(File.basename(path))
+            tempfile.write s3_object.read
+            tempfile.close
+
+            gz = Zlib::GzipReader.open(tempfile)
+            last_modified = gz.mtime
+            md5 = Digest::MD5.hexdigest(gz.read)
+          else
+            last_modified = s3_object.last_modified
+            md5 = s3_object.etag.gsub(/"/, '').split('-',2).first
+          end
           normalised_path = "./#{path}"
           Filey.new(normalised_path,
                     name,
-                    s3_object.last_modified,
-                    s3_object.etag.gsub(/"/, '').split('-',2).first)
+                    last_modified,
+                    md5)
         }
       end
     end
